f5bf490b7e3f2ea3be58f80b308fd66b250d30c5
Abhilasha Srivastava <abhilashasrivastava13@gmail.com>
2024-04-21 00:37:07 +0530

    commit

--- api/main-tf-serving.py
+++ api/main-tf-serving.py
@@ -1,5 +1,4 @@
 import numpy as np
-import requests
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
 import numpy as np
@@ -9,10 +8,9 @@ import tensorflow as tf
 
 app = FastAPI()
 
-endpoint = "http://localhost:8501/v1/models/Potato_models:predict"
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+MODEL = tf.keras.models.load_model("../saved_models/1")
 
-
+CLASS_NAMES = ["Early Blight", "Late Blight","Healthy"]
 @app.get("/ping")
 async def ping():
     return "hello neha"
@@ -29,19 +27,16 @@ async def predict(
 ):
     image = read_file_as_image(await file.read())
 
-    img_batch = np.expand_dims(image, 0)
-    json_data = {
-        "instances": img_batch.tolist()
+    img_batch = np.expand_dims(image,0)
+
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class' : predicted_class,
+        'confidence' : float(confidence)
     }
 
-    response = requests.post(endpoint, json=json_data)
-    prediction = np.array(response.json()["predictions"][0])
-    predicted_class = CLASS_NAMES[np.argmax(prediction)]
-    confidence = np.max(prediction)
 
-    return{
-        "class":predicted_class,
-        "confidence":confidence
-    }
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
